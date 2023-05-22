e5479ef37e2a47af94cb509f4cb01627e243407d
hanool <29415530+hanool@users.noreply.github.com>
2023-05-22 22:20:39 +0900

    add loop to input users number guessing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Guess the Number!");
+
+        let mut user_input = String::new();
+        let user_input = match io::stdin().read_line(&mut user_input) {
+            Ok(_) => user_input.trim(),
+            Err(error) => {
+                println!("Could not read input: {}", error.to_string());
+                break;
+            }
+        };
+
+        let guessed_number = match user_input.parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input valid number");
+                continue;
+            }
+        };
+
+        println!("You Guessed {}", guessed_number);
+    }
 }
