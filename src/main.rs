db8cb8f74205e1a387ce83bae0bc9fa471bf5f68
hanool <29415530+hanool@users.noreply.github.com>
2023-05-22 22:34:56 +0900

    add colorized text for better ui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
 use std::io;
@@ -15,7 +16,7 @@ fn main() {
         let user_input = match io::stdin().read_line(&mut user_input) {
             Ok(_) => user_input.trim(),
             Err(error) => {
-                println!("Could not read input: {}", error.to_string());
+                println!("Could not read input: {}", error.to_string().red());
                 break;
             }
         };
@@ -23,16 +24,16 @@ fn main() {
         let guessed_number = match user_input.parse::<i32>() {
             Ok(num) => num,
             Err(_) => {
-                println!("Please input valid number");
+                println!("{}", "Please input valid number".red());
                 continue;
             }
         };
 
         match guessed_number.cmp(&answer) {
-            Ordering::Less => println!("Too Small!"),
-            Ordering::Greater => println!("Too Big!"),
+            Ordering::Less => println!("{}", "Too Small!".yellow().bold()),
+            Ordering::Greater => println!("{}", "Too Big!".yellow().bold()),
             Ordering::Equal => {
-                println!("You Win!");
+                println!("{}", "You Win!".green().bold());
                 break;
             }
         }
