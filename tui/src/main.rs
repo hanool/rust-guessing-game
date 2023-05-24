43cecd786fd2491ef88726efa38e70f2bc79f709
hanool <29415530+hanool@users.noreply.github.com>
2023-05-24 17:41:00 +0900

    add tui hello, world feature

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    execute, terminal,
+};
+use std::io;
+
+fn run<W>(w: &mut W) -> io::Result<()>
+where
+    W: io::Write,
+{
+    execute!(w, terminal::EnterAlternateScreen)?;
+
+    terminal::enable_raw_mode()?;
+
     println!("Hello, world!");
+    println!("Press 'q' to quit!");
+
+    loop {
+        if let Ok(Event::Key(KeyEvent {
+            code: KeyCode::Char(c),
+            kind: KeyEventKind::Press,
+            modifiers: _,
+            state: _,
+        })) = event::read()
+        {
+            if c == 'q' {
+                println!("Goodbye, world!");
+                break;
+            }
+        }
+    }
+
+    terminal::disable_raw_mode()
+}
+
+fn main() -> std::io::Result<()> {
+    println!("Hello, world!");
+
+    let mut stdout = io::stdout();
+    run(&mut stdout)
 }
