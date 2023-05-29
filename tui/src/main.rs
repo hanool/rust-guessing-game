81f99bb027da2b57b438ed450c2ce5736c84b119
hanool <29415530+hanool@users.noreply.github.com>
2023-05-29 11:36:55 +0900

    add clean up terminal on leave, to be a good citizen

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,17 +1,21 @@
 use crossterm::{
+    cursor::{Hide, MoveTo},
     event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
-    execute, style::{self, Stylize}, terminal,
+    execute,
+    style::Stylize,
+    terminal,
 };
+use pad::{Alignment, PadStr};
 use std::io;
-use pad::{PadStr, Alignment};
 
 fn run<W>(w: &mut W) -> io::Result<()>
 where
     W: io::Write,
 {
+    // Initialize app
     execute!(w, terminal::EnterAlternateScreen)?;
-
     terminal::enable_raw_mode()?;
+    execute!(w, Hide, MoveTo(0, 0))?;
 
     display_title()?;
 
@@ -25,26 +29,32 @@ where
         }
     }
 
-    terminal::disable_raw_mode()
+    // Clean up app, be a good ciitzen
+    terminal::disable_raw_mode()?;
+    execute!(w, terminal::LeaveAlternateScreen)
 }
 
-fn display_title() -> io::Result<()>{
-
+fn display_title() -> io::Result<()> {
     let size = terminal::size()?;
     let width: usize = size.0.into();
     let title: &str = include_str!("../assets/title.txt");
     let title = title.split("\n");
 
     for row in title {
-        println!("{}", row
-             .pad_to_width_with_alignment(width, Alignment::Middle)
-             .stylize()
-             .bold());
+        println!(
+            "{}",
+            row.pad_to_width_with_alignment(width, Alignment::Middle)
+                .stylize()
+                .bold()
+        );
     }
 
-    println!("{}", "Press Enter to start!"
-             .pad_to_width_with_alignment(width, Alignment::Middle)
-             .slow_blink());
+    println!(
+        "{}",
+        "Press Enter to start!"
+            .pad_to_width_with_alignment(width, Alignment::Middle)
+            .slow_blink()
+    );
 
     Ok(())
 }
