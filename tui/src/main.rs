0c0973f73da9926e3fb5376fdc895710319af34f
hanool <29415530+hanool@users.noreply.github.com>
2023-05-29 14:28:57 +0900

    clean up left over messy codes

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,5 +1,5 @@
 use crossterm::{
-    cursor::{Hide, MoveTo},
+    cursor::{Hide, MoveTo, Show},
     event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
     execute,
     style::Stylize,
@@ -31,14 +31,18 @@ where
 
     // Clean up app, be a good ciitzen
     terminal::disable_raw_mode()?;
+    execute!(w, Show)?;
     execute!(w, terminal::LeaveAlternateScreen)
 }
 
 fn display_title() -> io::Result<()> {
-    let size = terminal::size()?;
-    let width: usize = size.0.into();
+    let width: usize = terminal::size().unwrap().0.into();
     let title: &str = include_str!("../assets/title.txt");
-    let title = title.split("\n");
+    let title = if title.contains("\r\n") {
+        title.split("\r\n")
+    } else {
+        title.split("\n")
+    };
 
     for row in title {
         println!(
@@ -51,7 +55,7 @@ fn display_title() -> io::Result<()> {
 
     println!(
         "{}",
-        "Press Enter to start!"
+        "Press 'q' to quit"
             .pad_to_width_with_alignment(width, Alignment::Middle)
             .slow_blink()
     );
